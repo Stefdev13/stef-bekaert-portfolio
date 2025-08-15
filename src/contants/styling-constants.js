b29e0c1ad0849d2610b023676dfbf493f9f0b4cd
EikcoDev <stef.bekaert@eikco.org>
2025-08-15 13:42:48 +0200

    Global constants
    Added a constants folder and a styling-constants file with colours and fonts to be used in .jsx files
    Added global styling constants is the :root css tag

---
+++ src/contants/styling-constants.js
@@ -0,0 +1,44 @@
+//This constants file holds the colour variables for use in .jsx files, the styling constants for use in the .css files are in the index.css file
+//===== Fonts =====
+export const FONT_BRICOLAGE_GROTESQUE = "Bricolage_Grotesque"
+export const FONT_QUICKSAND = "Quicksand"
+export const FONT_IBM_PLEX_MONO = "IBM_Plex_Mono_Regular"
+export const PACIFICO = "Pacifico"
+
+//===== Colours =====
+//Dark mode
+export const DARK_BG_L1 = '#000000'
+export const DARK_BG_L2 = '#1A1A1A'
+export const DARK_BG_L3 = '#333333'
+export const DARK_BORDER = '#666666'
+export const DARK_TEXT_HEADER = '#F2F2F2'
+export const DARK_TEXT_MUTED = '#D9D9D9'
+
+//Light mode
+export const LIGHT_BG_L1 = '#FFFFFF'
+export const LIGHT_BG_L2 = '#FAF9F5'
+export const LIGHT_BG_L3 = '#EEEBDD'
+export const LIGHT_BORDER = '#DED8BA'
+export const LIGHT_TEXT_HEADER = '#333333'
+export const LIGHT_TEXT_MUTED = '#4D4D4D'
+
+//Accent & other colours
+export const ACCENT_PRIMARY_LIGHT = '#E2F3F3'
+export const ACCENT_PRIMARY = '#39C6C6'
+export const ACCENT_PRIMARY_DARK = '#2E6B6B'
+
+export const ACCENT_DANGER_LIGHT = '#FBE9E9'
+export const ACCENT_DANGER = '#D14747'
+export const ACCENT_DANGER_DARK = '#AD1F1F'
+
+export const ACCENT_WARNING_LIGHT = '#FFF5E7'
+export const ACCENT_WARNING = '#FFAA33'
+export const ACCENT_WARNING_DARK = '#EC9213'
+
+export const ACCENT_SUCCESS_LIGHT = '#EDF7F1'
+export const ACCENT_SUCCESS = '#40BF6A'
+export const ACCENT_SUCCESS_DARK = '#1F7A3D'
+
+export const ACCENT_INFO_LIGHT = '#EEEEFC'
+export const ACCENT_INFO = '#6666CC'
+export const ACCENT_INFO_DARK = '#4D4DB2'
