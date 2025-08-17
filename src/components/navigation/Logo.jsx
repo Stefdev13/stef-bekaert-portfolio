7410eeb4a52ce3bf9ae884326dfa697d0a400134
Stefdev13 <stef.bekaert@proton.me>
2025-08-17 12:47:44 +0200

    Logo
    Dont do the effect if we're on the homepage

--- src/components/navigation/Logo.jsx
+++ src/components/navigation/Logo.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import styles from "./Logo.module.css";
-import { useNavigate } from "react-router";
+import { useNavigate, useLocation } from "react-router";
 
 function Logo() {
   let navigate = useNavigate();
+  const location = useLocation();
 
   const [text, setText] = useState("stef.bekaert");
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -21,60 +22,62 @@ function Logo() {
   }
 
   function doEffect(newWord) {
-    let i = 0;
+    if (location.pathname !== "/") {
+      let i = 0;
 
-    //2 variables
-    //Used to determine which word is the longer and to add extra letters if the next word is longer to smoothe out the animation
-    let differenceInLetters;
-    let newTextValue;
+      //2 variables
+      //Used to determine which word is the longer and to add extra letters if the next word is longer to smoothe out the animation
+      let differenceInLetters;
+      let newTextValue;
 
-    const interval = setInterval(() => {
-      let originalText = text;
-      let newLetter = "";
+      const interval = setInterval(() => {
+        let originalText = text;
+        let newLetter = "";
 
-      //Check if the difference in letters has been set, and set it if it hasnt
-      if (differenceInLetters === undefined) {
-        differenceInLetters = text.length - newWord.length;
-      }
+        //Check if the difference in letters has been set, and set it if it hasnt
+        if (differenceInLetters === undefined) {
+          differenceInLetters = text.length - newWord.length;
+        }
 
-      //Check if the difference in letters has been set, and set it if it hasnt
-      if (newTextValue === undefined) {
-        newTextValue = text;
-      }
+        //Check if the difference in letters has been set, and set it if it hasnt
+        if (newTextValue === undefined) {
+          newTextValue = text;
+        }
 
-      //Get a new letter to add to the end of the text if the difference in letters is negative (To smoothe out the animation)
-      if (differenceInLetters < 0) {
-        //Add a random letter from the array of letters to the text
-        newLetter = letters[Math.floor(Math.random() * letters.length)];
-        differenceInLetters++;
-      } else {
-        newLetter = "";
-      }
+        //Get a new letter to add to the end of the text if the difference in letters is negative (To smoothe out the animation)
+        if (differenceInLetters < 0) {
+          //Add a random letter from the array of letters to the text
+          newLetter = letters[Math.floor(Math.random() * letters.length)];
+          differenceInLetters++;
+        } else {
+          newLetter = "";
+        }
 
-      newTextValue = newTextValue
-        .split("")
-        .map((letter, index) => {
-          if (index < i) {
-            return newWord[index];
-          }
+        newTextValue = newTextValue
+          .split("")
+          .map((letter, index) => {
+            if (index < i) {
+              return newWord[index];
+            }
 
-          return letters[Math.floor(Math.random() * letters.length)];
-        })
-        .join("")
-        .concat(newLetter);
+            return letters[Math.floor(Math.random() * letters.length)];
+          })
+          .join("")
+          .concat(newLetter);
 
-      setText(newTextValue);
+        setText(newTextValue);
 
-      if (i >= originalText.length) {
-        //Update the state
-        setText(newWord);
+        if (i >= originalText.length) {
+          //Update the state
+          setText(newWord);
 
-        //Clear the interval
-        clearInterval(interval);
-      }
+          //Clear the interval
+          clearInterval(interval);
+        }
 
-      i += 1 / 2;
-    }, 10);
+        i += 1 / 2;
+      }, 10);
+    }
   }
 
   return (
