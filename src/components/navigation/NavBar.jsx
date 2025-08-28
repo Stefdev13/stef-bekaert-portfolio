d69c9f0a84aa7f7c422acbc02abdcddf54f9cc56
Stefdev13 <stef.bekaert@proton.me>
2025-08-28 17:37:35 +0200

    Navbar
    Making it responsive part 2 (almost done)

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router";
 import styles from "./NavBar.module.css";
 import LightDarkModeToggle from "../LightDarkModeToggle";
@@ -8,23 +8,53 @@ import menuBurgerLight from "../../assets/images/menu-burger-light.png";
 
 function NavBar() {
   const theme = useTheme();
+  const [isNavbarUnfurled, setIsNavbarUnfurled] = useState(false);
+
   return (
     <div className={styles.row}>
-      <LightDarkModeToggle />
-      <div className={styles.navbarToggle}>
-        <img src={theme ? menuBurgerDark : menuBurgerLight} alt="menu icon" />
+      <div className={styles.toggleRow}>
+        <LightDarkModeToggle />
+        <div
+          className={styles.navbarToggle}
+          onClick={() => {
+            setIsNavbarUnfurled(!isNavbarUnfurled);
+          }}
+        >
+          <img src={theme ? menuBurgerDark : menuBurgerLight} alt="menu icon" />
+        </div>
       </div>
+
       <div className={styles.navbar}>
-        <NavLink to="/" className={styles.navLink}>
+        <NavLink
+          to="/"
+          className={`${styles.navLink} ${
+            isNavbarUnfurled ? styles.unfurled : ""
+          }`}
+        >
           Home
         </NavLink>
-        <NavLink to="/about" className={styles.navLink}>
+        <NavLink
+          to="/about"
+          className={`${styles.navLink} ${
+            isNavbarUnfurled ? styles.unfurled : ""
+          }`}
+        >
           About Me
         </NavLink>
-        <NavLink to="/work" className={styles.navLink}>
+        <NavLink
+          to="/work"
+          className={`${styles.navLink} ${
+            isNavbarUnfurled ? styles.unfurled : ""
+          }`}
+        >
           Work
         </NavLink>
-        <NavLink to="/contact" className={styles.contactBtn}>
+        <NavLink
+          to="/contact"
+          className={`${styles.navLink} ${
+            isNavbarUnfurled ? styles.unfurled : ""
+          }`}
+        >
           Contact me
         </NavLink>
       </div>
