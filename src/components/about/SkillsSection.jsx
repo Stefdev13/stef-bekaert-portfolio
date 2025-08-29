b41d2294caac843fabf1d0f1215c1ca692e4bdee
Stefdev13 <stef.bekaert@proton.me>
2025-08-29 22:37:12 +0200

    About page
    Added the background gradient on mobile dark mode

--- src/components/about/SkillsSection.jsx
+++ src/components/about/SkillsSection.jsx
@@ -2,10 +2,16 @@ import KnotAndTitle from "./KnotAndTitle";
 import SkillsList from "./SkillsList";
 import * as Constants from "../../constants/skills-constants";
 import styles from "../../pages/AboutPage.module.css";
+import { useTheme } from "../../context/ThemeProvider";
 
 function SkillsSection() {
+  const theme = useTheme();
   return (
-    <div className={styles.sectionWrapper}>
+    <div
+      className={`${styles.sectionWrapper} ${
+        theme ? "" : styles.skillsSection
+      }`}
+    >
       <KnotAndTitle title="My Skills" location="middle" />
       <div className={styles.contentAndLineWrapper}>
         <div className={styles.line} />
