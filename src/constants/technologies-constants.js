24beaf2ad04a552ac76c873125dc1de2d124e9e8
Stefdev13 <stef.bekaert@proton.me>
2025-09-27 22:41:39 +0200

    Added technologies constants
    Added the relevant technologies and an icon for each one

---
+++ src/constants/technologies-constants.js
@@ -0,0 +1,88 @@
+export const HTML = {
+    name: "HTML",
+    icon: "/images/technoglogies/html.svg",
+}
+export const CSS = {
+    name: "CSS",
+    icon: "/images/technoglogies/css.svg",
+}
+export const ANGULAR = {
+    name: "Angular",
+    icon: "/images/technoglogies/angular.svg",
+}
+export const BOOTSTRAP = {
+    name: "Bootstrap",
+    icon: "/images/technoglogies/bootstrap.svg",
+}
+export const CSHARP = {
+    name: "C#",
+    icon: "/images/technoglogies/c-sharp.png",
+}
+export const DART = {
+    name: "Dart",
+    icon: "/images/technoglogies/dart.svg",
+}
+export const FIREBASE = {
+    name: "Firebase",
+    icon: "/images/technoglogies/firebase.svg",
+}
+export const FLUTTER = {
+    name: "Flutter",
+    icon: "/images/technoglogies/flutter.svg",
+}
+export const JAVA = {
+    name: "Java",
+    icon: "/images/technoglogies/java.svg",
+}
+export const JAVASCRIPT = {
+    name: "JavaScript",
+    icon: "/images/technoglogies/javascript.svg",
+}
+export const KOTLIN = {
+    name: "Kotlin",
+    icon: "/images/technoglogies/kotlin.svg",
+}
+export const LARAVEL = {
+    name: "Laravel",
+    icon: "/images/technoglogies/laravel.svg",
+}
+export const MONGO_DB = {
+    name: "MongoDB",
+    icon: "/images/technoglogies/mongo-db.svg",
+}
+export const MYSQL = {
+    name: "MySQL",
+    icon: "/images/technoglogies/mysql.svg",
+}
+export const NODE_JS = {
+    name: "Node.js",
+    icon: "/images/technoglogies/node-js.svg",
+}
+export const PHP = {
+    name: "php",
+    icon: "/images/technoglogies/php.svg",
+}
+export const PYTHON = {
+    name: "Python",
+    icon: "/images/technoglogies/python.svg",
+}
+export const REACT_ROUTER = {
+    name: "React-Router",
+    icon: "/images/technoglogies/react-router.svg",
+}
+export const REACT = {
+    name: "React",
+    icon: "/images/technoglogies/react.svg",
+}
+export const TAILWIND = {
+    name: "Tailwind",
+    icon: "/images/technoglogies/tailwind.svg",
+}
+export const TYPESCRIPT = {
+    name: "TypeScript",
+    icon: "/images/technoglogies/typescript.svg",
+}
+export const VUE = {
+    name: "Vue.js",
+    icon: "/images/technoglogies/vue.svg",
+}
