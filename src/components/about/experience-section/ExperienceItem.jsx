c5fd85c8626e0d5a7b5b1f418dbc71b7e70de45c
Stefdev13 <stef.bekaert@proton.me>
2025-10-06 11:51:05 +0200

    ExperienceItem technology list
    Added dynamic styling to update the layout when the list of technologies gets too wide relative to the entire ExperienceItem

--- src/components/about/experience-section/ExperienceItem.jsx
+++ src/components/about/experience-section/ExperienceItem.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "./ExperienceItem.module.css";
 import ExperienceItemDetailView from "./ExperienceItemDetailView";
 
@@ -6,6 +6,52 @@ function ExperienceItem(props) {
   const item = props.item;
   const isEven = props.isEven;
   const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+
+  const experienceItemWrapperRef = useRef();
+  const techListWrapperRef = useRef();
+  const techListRef = useRef();
+
+  useEffect(() => {
+    //React calls the useEffect before the browser paints the screen, which can make the width incorrect
+    //  To fix this: use setTimeout to make this run after the paint
+    setTimeout(() => {
+      updateLayout();
+    }, 0);
+
+    function updateLayout() {
+      console.log("in func");
+      //Should only update the layout when on desktop or tablet, mobile is always in column
+      if (window.innerWidth >= 992) {
+        let wrapperWidth =
+          experienceItemWrapperRef.current.getBoundingClientRect().width;
+        let techItemsWidth = techListRef.current.getBoundingClientRect().width;
+
+        console.log("techItemsWidth");
+        console.log(techItemsWidth);
+        console.log("wrapperWidth");
+        console.log(wrapperWidth);
+        console.log(techItemsWidth / wrapperWidth > 0.54);
+
+        //If the list of technologies is too long we want to put in on a line below the name and item
+        if (techItemsWidth / wrapperWidth > 0.54) {
+          experienceItemWrapperRef.current.style.setProperty(
+            "flex-direction",
+            "column"
+          );
+        } else {
+          experienceItemWrapperRef.current.style.setProperty(
+            "flex-direction",
+            "row"
+          );
+        }
+      }
+    }
+
+    //We need the layout to be updated when the browser is resized
+    window.addEventListener("resize", updateLayout);
+
+    return () => window.removeEventListener("resize", updateLayout);
+  }, []);
 
   function handleOnItemClick() {
     setIsOverlayOpen(!isOverlayOpen);
@@ -33,24 +79,36 @@ function ExperienceItem(props) {
       <div
         className={`${styles.wrapper} ${isEven ? styles.even : styles.odd}`}
         onClick={handleOnItemClick}
+        ref={experienceItemWrapperRef}
       >
-        <div
-          className={`${styles.typeTag} ${
-            item.type.toLowerCase() == "course" ? styles.course : styles.project
-          }`}
-        >
-          {item.type}
+        <div className={styles.tagAndName}>
+          <div
+            className={`${styles.typeTag} ${
+              item.type.toLowerCase() == "course"
+                ? styles.course
+                : styles.project
+            }`}
+          >
+            {item.type}
+          </div>
+          <p className={styles.name}>{item.name}</p>
         </div>
-        <p className={styles.name}>{item.name}</p>
-        <div className={styles.technologiesList}>
-          {item.technologyList.map(function renderTechnologies(technology) {
-            return (
-              <div key={technology.name} className={styles.technologyTag}>
-                <img src={technology.icon} alt={technology.name} />
-                <p>{technology.name}</p>
-              </div>
-            );
-          })}
+
+        <div className={styles.techListWrapper} ref={techListWrapperRef}>
+          <ul
+            className={styles.technologiesList}
+            ref={techListRef}
+            id="TechList"
+          >
+            {item.technologyList.map(function renderTechnologies(technology) {
+              return (
+                <div key={technology.name} className={styles.technologyTag}>
+                  <img src={technology.icon} alt={technology.name} />
+                  <p>{technology.name}</p>
+                </div>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </div>
